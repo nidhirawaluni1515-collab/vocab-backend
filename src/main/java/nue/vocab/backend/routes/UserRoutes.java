8d00b5c465aec4cec730aa072ef0fa0006d5c7ca
Nidhi Rawal <nidhirawal@mac.lan>
2026-03-02 23:47:52 -0800

    routes

--- src/main/java/nue/vocab/backend/routes/UserRoutes.java
+++ src/main/java/nue/vocab/backend/routes/UserRoutes.java
@@ -6,5 +6,6 @@ public class UserRoutes {
     public static final String GET_ALL    = BASE + "/all";
     public static final String GET_BY_ID  = BASE + "/{id}";
     public static final String CREATE     = BASE + "/add";
+    public static final String LOGIN     = BASE + "/login";
     public static final String DELETE     = BASE + "/remove/{id}";
 }
